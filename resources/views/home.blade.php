21c66afc0fb1f5d0f407f5f33d564520672b4cf7
malickateeq <malickateeq@gmail.com>
2019-07-08 14:18:55 +0500

    initial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,25 @@
 <div class="container">
 
     <div class="card-body">
-        {{ $settings }} <br>
-        {{ $user }}
+        <ul>
+            <h1> User Profile </h1>
+            <li>  <b>Name: </b> {{$user->name}} </li>
+            <li>  <b>Email: </b> {{$user->email}} </li>
+
+            <h1> Site Settings </h1>
+            <li>  <b>Title: </b> {{$settings->title}} </li>
+            <li>  <b>Description: </b> {{$settings->description}} </li>
+            <li>  <b>One liner: </b> {{$settings->one_liner}} </li>
+            <li>  <b>Phone: </b> {{$settings->phone}} </li>
+            <li>  <b>Address: </b> {{$settings->address}} </li>
+            <li>  <b>Facebook link: </b> {{$settings->fb_link}} </li>
+            <li>  <b>Instagram link: </b> {{$settings->insta_link}} </li>
+            <li>  <b>Twitter link: </b> {{$settings->twitter_link}} </li>
+            <li>  <b>Youtube link: </b> {{$settings->youtube_link}} </li>
+            <li>  <b>Other links: </b> {{$settings->other_links}} </li>
+            <h3> Background Image </h3>
+            <li>  <b>Background Image: </b> {{$settings->bg_image}} </li>
+        </ul>
     </div>
 
 </div>

29b2749df94d59659ac46638eee6c08638bc0d34
malickateeq <malickateeq@gmail.com>
2019-07-11 11:37:00 +0500

    initial

--- app/Settings.php
+++ app/Settings.php
@@ -9,7 +9,8 @@ class Settings extends Model
     //
     protected $fillable = [
         'bg_image', 'favicon', 'title', 'name', 'description', 'phone', 'one_liner',
-        'address', 'fb_link', 'twitter_link', 'insta_link', 'youtube_link', 'other_links'
+        'address', 'fb_link', 'twitter_link', 'insta_link', 'youtube_link', 'other_links',
+        'btn1_text', 'btn1_link', 'btn2_text', 'btn2_link', 'btn3_text', 'btn3_link', 
     ];
 
 }
